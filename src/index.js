dc0f1ebe0204edd1fd2bb3f936c4da6c52d84123
Michell Alegria <alegria.trm@gmail.com>
2018-08-22 16:00:41 -0500

    context

--- src/index.js
+++ src/index.js
@@ -1,42 +1,63 @@
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-const noop = () => {};
+const noop = () => {}
 
-export default class MockRouter extends React.Component {
-
-  getChildContext() {
-    return ({
-      router: {
-        route: {
-          location: this.props.location,
-          match: {
-            url: this.props.url,
-            params: this.props.params,
-            path: this.props.path
-          }
-        },
-        history: {
-          path: this.props.path,
-          createHref: this.props.createHref,
-          push: this.props.push,
-          replace: this.props.replace
-        }
+const MockRouterContext = React.createContext({
+  router: {
+    route: {
+      location: '',
+      match: {
+        url: '',
+        params: {},
+        path: ''
+      },
+      history: {
+        path: '',
+        createHref: noop,
+        push: noop,
+        replace: noop
       }
-    });
+
+    }
   }
 
-  render() {
+})
+
+export default class MockRouter extends React.Component {
+  render () {
+    const routerValue =
+      {
+        router: {
+          route: {
+            location: this.props.location,
+            match: {
+              url: this.props.url,
+              params: this.props.params,
+              path: this.props.path
+            }
+          },
+          history: {
+            path: this.props.path,
+            createHref: this.props.createHref,
+            push: this.props.push,
+            replace: this.props.replace
+          }
+        }
+      }
+
     return (
-      React.Children.only(this.props.children)
-    );
+      <MockRouterContext.Provider value={routerValue} >
+        {React.Children.only(this.props.children)}
+      </MockRouterContext.Provider>
+    )
   }
 }
 
 MockRouter.childContextTypes = {
   router: PropTypes.object
-};
+}
 
 MockRouter.defaultProps = {
   url: '',
